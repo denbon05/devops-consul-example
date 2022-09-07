07e9dc6ecc791f1beb92e13c3a8c0d85e0c825cf
feycot <feycot@gmail.com>
2022-09-07 18:33:55 +0500

    init

---
+++ terraform/servers.tf
@@ -0,0 +1,12 @@
+resource "digitalocean_droplet" "servers" {
+  count              = 2
+  image              = "docker-20-04"
+  name               = "service-discovery-${count.index + 1}"
+  region             = "ams3"
+  size               = "s-1vcpu-1gb"
+  private_networking = true
+
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+}
---
+++ terraform/ssh.tf
@@ -0,0 +1,8 @@
+# Ключ можно либо получить созданный, либо создать новый
+# resource "digitalocean_ssh_key" "default" {
+#   name       = "Terraform Homework"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
+data "digitalocean_ssh_key" "terraform" {
+  name = "key"
+}
